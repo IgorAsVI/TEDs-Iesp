a39d2ae15a218d54e7e8d4648b8876cf5cc19b7e
IgorAsVI <igorassisvilar@gmail.com>
2022-04-09 12:25:53 -0300

    Atividades complementares

--- Ted1/main.java
+++ Ted1/main.java
@@ -1,5 +1,8 @@
 package Ted1;
 
+import Ted1.Atividade1.Atv;
+import Ted1.Atv2.Emprestimo;
+
 public class main {
     Produto pPro = new Produto("123","mt bom",100,80,true);
     Produto pNaoPro = new Produto("123","mt bom",100,70,false);
@@ -7,14 +10,15 @@ public class main {
     Venda venda = new Venda(pNaoPro,v,0.1,2,400);
 
     public static void main(String[] args) {
-        Produto pPro = new Produto("123","mt bom",100,70,true);
-        Produto pNaoPro = new Produto("123","mt bom",100,70,false);
-        Vendedor v = new Vendedor("igor","V01","Logo ali");
-        Venda venda = new Venda(pNaoPro,v,0.1,2,400);
+       // Atv.salário();
+        Emprestimo em = new Emprestimo();
+        em.setIdade(15);
+        em.setMesesParaPagar(8);
+        em.setNomeUsuario("sada");
+        em.setPercentualJurosMes(10);
+        em.setValorEmprestimo(100);
+        em.imprimir();
+        System.out.println(em.calcularEmprestimo());
 
-        venda.calculaValor();
-        venda.calcularComissao();
-        venda.exibe();
-        venda.efetuarDesconto(90);
     }
 }
